63743b18f8f025af764aebc1fe30bfb5a22f10ef
muraterdogann <muraterdogann05@gmail.com>
2024-11-02 02:36:34 +0300

    menu fixed

--- component/layouts/header.tsx
+++ component/layouts/header.tsx
@@ -106,6 +106,25 @@ export default function Header04() {
       },
       {
         id: uuidv4(),
+        name: "SEO Yönetimi",
+        path: "/hizmetlerimiz/seo-yonetimi",
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#09244B"
+              d="M19.76 10.77l-.09-.35h-7.44v3.16h4.45a4.45 4.45 0 01-4.36 3.34 5.21 5.21 0 01-3.5-1.39A5 5 0 017.33 12a5.14 5.14 0 011.46-3.53 5 5 0 013.48-1.37 4.55 4.55 0 013 1.16L17.47 6a7.88 7.88 0 00-5.27-2 8.14 8.14 0 00-5.77 2.35 8.15 8.15 0 00-.09 11.21 8.37 8.37 0 006 2.44 7.45 7.45 0 005.41-2.27 8 8 0 002.08-5.54 9.88 9.88 0 00-.07-1.42z"
+            ></path>
+          </svg>
+        ),
+      },
+      {
+        id: uuidv4(),
         name: "E-ticaret Web Site Kurulumu",
         path: "/hizmetlerimiz/e-ticaret-web-site-kurulumu",
         icon: (
@@ -319,7 +338,7 @@ export default function Header04() {
                   </i>
                 </button>
                 <ul
-                  className="dropdown-menu -left-6 top-[85%] z-10 hidden grid-flow-col grid-rows-4 gap-x-4 whitespace-nowrap rounded-xl bg-white transition-all will-change-transform group-hover:visible group-hover:opacity-100 lg:invisible lg:absolute lg:!grid lg:translate-y-4 lg:py-8 lg:px-5 lg:opacity-0 lg:shadow-2xl lg:group-hover:translate-y-2 relative"
+                  className="dropdown-menu -left-6 top-[85%] z-10 hidden grid-flow-col grid-rows-5 gap-x-4 whitespace-nowrap rounded-xl bg-white transition-all will-change-transform group-hover:visible group-hover:opacity-100 lg:invisible lg:absolute lg:!grid lg:translate-y-4 lg:py-8 lg:px-5 lg:opacity-0 lg:shadow-2xl lg:group-hover:translate-y-2 relative"
                   aria-labelledby="navDropdown-1"
                 >
                   {hizmetlerimiz?.pages?.map((page) => (

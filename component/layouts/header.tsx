55ee6a0a7338e3d883afbb0183ca4359f1cb338f
muraterdogann <muraterdogann05@gmail.com>
2024-11-02 05:47:53 +0300

    menu ve header tamam

--- component/layouts/header.tsx
+++ component/layouts/header.tsx
@@ -12,16 +12,24 @@ export default function Header04() {
   const [toggle, setToggle] = useState(false);
   const [isCollapse, setCollapse] = useState<string | number | null>(null);
   const [isScroll, setScroll] = useState(false);
+  const [menuPosition, setMenuPosition] = useState("top-16");
 
-  // sticky menu
+  // scroll event for menu positioning
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 4) {
+    const handleScroll = () => {
+      if (window.scrollY > 16) {
         setScroll(true);
+        setMenuPosition("top-0"); // menu goes to the top of the page when scrolling down
       } else {
         setScroll(false);
+        setMenuPosition("top-16"); // menu returns to its original position when at the top of the page
       }
-    });
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   // window resize
@@ -33,19 +41,13 @@ export default function Header04() {
     });
   });
 
-  type ServicePage = {
-    id: string,
-    name: string,
-    path: string,
-
-    condition?: boolean;
-  }
-
-  type Service = {
-    id: number,
-    name: string,
-    pages: ServicePage[],
-  }
+  // Function for mobile collapse
+  const mobileCollapse = (id: string | number) => {
+    if (isCollapse === id) {
+      return setCollapse(null); // Reset collapse state
+    }
+    setCollapse(id);
+  };
 
 
   /* -------------------------------------------------------------------------- */
@@ -261,22 +263,117 @@ export default function Header04() {
     ],
   };
 
-  const mobileCollapse = (id: string | number) => {
-
-    if (isCollapse === id) {
-      return setCollapse(null); // setCollapse fonksiyonuna null geçiliyor
-    }
-    setCollapse(id);
-
-  };
-
   return <>
+    {/* Masaüstü Top Bar (1024px ve üstü için) */}
+    <div className="pamTopBar-container top-0 w-full z-20 hidden md:block">
+      <div className="pamTopBar-background bg-black font-poppins select-none overflow-hidden">
+        <div className="pamTopBar-content flex justify-center items-center p-2.5 text-base leading-6 text-gray-300">
+          <a
+            href="/iletisim"
+            className="pamTopBar-link cursor-pointer font-medium text-center bg-clip-text text-transparent bg-gradient-to-r from-[#00C896] via-[#27A199] to-[#00C896] bg-[length:300%] animate-pulse"
+          >
+            ⚡ Reklam bütçeniz boşa gitmesin; satışlarınızı Pam Ajans ile katlayın!
+          </a>
+          <div className="pamTopBar-icon line-height-0 mx-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 21 11"
+              fill="none"
+            >
+              <path
+                d="M1 5.38462L20 5.38462M20 5.38462L15.6981 1M20 5.38462L15.6981 9.76923"
+                stroke="#D1D5DB"
+                strokeWidth="1.09615"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
+          </div>
+          <a
+            href="tel:08503057726"
+            className="pamTopBar-phone cursor-pointer font-semibold text-base inline-block bg-clip-text text-transparent bg-gradient-to-r from-[#00C896] via-[#27A199] to-[#00C896] bg-[length:300%] animate-pulse"
+          >
+            0850 305 77 26
+          </a>
+          <div className="pamTopBar-or mx-9 text-gray-300 whitespace-pre">ya da</div>
+          <a
+            href="/iletisim"
+            className="pamTopBar-callback cursor-pointer flex justify-center items-center font-medium h-9 text-gray-300 transition duration-500 border border-gray-300 rounded-full px-4 whitespace-pre"
+          >
+            <div className="pamTopBar-callback-icon mr-2.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+              >
+                <path
+                  d="M10.9104 4.05809C11.6586 4.20378 12.3462 4.56898 12.8852 5.10696C13.4242 5.64494 13.7901 6.33121 13.9361 7.07795M10.9104 1C12.4648 1.17235 13.9143 1.8671 15.0209 2.97018C16.1275 4.07326 16.8254 5.5191 17 7.0703M16.234 13.1712V15.4648C16.2349 15.6777 16.1912 15.8884 16.1057 16.0835C16.0203 16.2786 15.8949 16.4537 15.7377 16.5977C15.5805 16.7416 15.3949 16.8512 15.1928 16.9194C14.9908 16.9876 14.7766 17.013 14.5642 16.9938C12.2071 16.7382 9.94297 15.9343 7.95371 14.6467C6.10295 13.4729 4.53384 11.9068 3.35779 10.0596C2.06326 8.0651 1.25765 5.79431 1.00622 3.43118C0.987076 3.21976 1.01225 3.00669 1.08014 2.80551C1.14802 2.60434 1.25713 2.41948 1.40052 2.2627C1.54391 2.10592 1.71843 1.98066 1.91298 1.89489C2.10753 1.80912 2.31785 1.76472 2.53053 1.76452H4.82849C5.20022 1.76087 5.56061 1.89226 5.84247 2.13419C6.12433 2.37613 6.30843 2.71211 6.36046 3.0795C6.45745 3.81349 6.63732 4.53418 6.89665 5.22781C6.99971 5.50145 7.02201 5.79884 6.96092 6.08474C6.89983 6.37065 6.7579 6.63308 6.55195 6.84095L5.57915 7.81189C6.66958 9.7259 8.25739 11.3107 10.1751 12.399L11.1479 11.4281C11.3561 11.2225 11.6191 11.0809 11.9055 11.0199C12.192 10.9589 12.4899 10.9812 12.7641 11.084C13.4591 11.3429 14.1811 11.5224 14.9165 11.6192C15.2886 11.6716 15.6284 11.8587 15.8713 12.1448C16.1143 12.431 16.2433 12.7962 16.234 13.1712Z"
+                  stroke="#D1D5DB"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            </div>
+            <div className="pamTopBar-callback-text text-base">Sizi Arayalım</div>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    {/* Mobil Top Bar (1024px altı için) */}
+    <div className="pamMobileTopBar-container bg-black text-white md:hidden">
+      <div className="pamMobileTopBar-content flex justify-between items-center text-xs px-5">
+        <div className="pamMobileTopBar-info flex flex-col py-1">
+          <Link
+            href="/iletisim"
+            className="pamMobileTopBar-link font-medium text-xs bg-clip-text text-transparent bg-gradient-to-r from-[#00C896] via-[#27A199] to-[#00C896] animate-pulse text-left"
+          >
+            ⚡ Reklam bütçeniz boşa gitmesin; satışlarınızı Pam Ajans ile katlayın!
+          </Link>
+          <a
+            href="tel:08503057726"
+            className="pamMobileTopBar-phone text-white font-semibold mt-1"
+          >
+            0850 305 77 26
+          </a>
+        </div>
+        <div className="pamMobileTopBar-callback flex-shrink-0">
+          <Link
+            href="/iletisim"
+            className="pamMobileTopBar-button flex items-center justify-center border border-white rounded-full px-2 py-1 hover:bg-white hover:text-black transition-all duration-200"
+          >
+            <div className="pamMobileTopBar-button-icon mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+              >
+                <path
+                  d="M10.9104 4.05809C11.6586 4.20378 12.3462 4.56898 12.8852 5.10696C13.4242 5.64494 13.7901 6.33121 13.9361 7.07795M10.9104 1C12.4648 1.17235 13.9143 1.8671 15.0209 2.97018C16.1275 4.07326 16.8254 5.5191 17 7.0703M16.234 13.1712V15.4648C16.2349 15.6777 16.1912 15.8884 16.1057 16.0835C16.0203 16.2786 15.8949 16.4537 15.7377 16.5977C15.5805 16.7416 15.3949 16.8512 15.1928 16.9194C14.9908 16.9876 14.7766 17.013 14.5642 16.9938C12.2071 16.7382 9.94297 15.9343 7.95371 14.6467C6.10295 13.4729 4.53384 11.9068 3.35779 10.0596C2.06326 8.0651 1.25765 5.79431 1.00622 3.43118C0.987076 3.21976 1.01225 3.00669 1.08014 2.80551C1.14802 2.60434 1.25713 2.41948 1.40052 2.2627C1.54391 2.10592 1.71843 1.98066 1.91298 1.89489C2.10753 1.80912 2.31785 1.76472 2.53053 1.76452H4.82849C5.20022 1.76087 5.56061 1.89226 5.84247 2.13419C6.12433 2.37613 6.30843 2.71211 6.36046 3.0795C6.45745 3.81349 6.63732 4.53418 6.89665 5.22781C6.99971 5.50145 7.02201 5.79884 6.96092 6.08474C6.89983 6.37065 6.7579 6.63308 6.55195 6.84095L5.57915 7.81189C6.66958 9.7259 8.25739 11.3107 10.1751 12.399L11.1479 11.4281C11.3561 11.2225 11.6191 11.0809 11.9055 11.0199C12.192 10.9589 12.4899 10.9812 12.7641 11.084C13.4591 11.3429 14.1811 11.5224 14.9165 11.6192C15.2886 11.6716 15.6284 11.8587 15.8713 12.1448C16.1143 12.431 16.2433 12.7962 16.234 13.1712Z"
+                  stroke="#FFFFFF"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <span className="pamMobileTopBar-button-text text-xs">Sizi Arayalım</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+
     {/* main desktop menu sart*/}
     <header
-      className={`js-page-header fixed top-0 z-20 w-full backdrop-blur transition-colors ${isScroll ? "js-page-header--is-sticky" : ""
+      className={`js-page-header fixed ${menuPosition} z-30 w-full transition-all duration-300 ${isScroll ? "bg-white shadow-lg" : ""
         }`}
     >
-      <div className={`flex items-center px-6 py-6 xl:px-24 ${isScroll ? 'dark' : ''}`}>
+      <div className={`flex items-center px-6 py-2 xl:px-24 ${isScroll ? 'dark' : ''}`}>
         <Link className="shrink-0" href="/" >
           <div className="dark:hidden">
             <Image
@@ -398,7 +495,7 @@ export default function Header04() {
                   <span className="text-white font-display hover:text-accentPam focus:text-accentPam flex items-center justify-between py-3.5 text-base dark:text-jacarta-700 lg:px-5 w-full cursor-pointer dark:hover:text-accentPam dark:focus:text-accentPam">
                     <span
                     >
-                     İletişim
+                      İletişim
                     </span>
                   </span>
                 </Link>
@@ -488,6 +585,16 @@ export default function Header04() {
       <nav className="navbar w-full mt-24">
         <ul className="flex flex-col lg:flex-row">
           <li className="group relative " onClick={() => setToggle(false)}>
+            <Link href="/">
+              <span className="text-jacarta-700 font-display hover:text-accentPam focus:text-accentPam flex items-center justify-between py-3.5 text-base dark:text-jacarta-700 lg:px-5 w-full cursor-pointer">
+                <span
+                >
+                  Ana Sayfa
+                </span>
+              </span>
+            </Link>
+          </li>
+          <li className="group relative " onClick={() => setToggle(false)}>
             <Link href="/hakkimizda">
               <span className="text-jacarta-700 font-display hover:text-accentPam focus:text-accentPam flex items-center justify-between py-3.5 text-base dark:text-jacarta-700 lg:px-5 w-full cursor-pointer">
                 <span
@@ -562,6 +669,16 @@ export default function Header04() {
                 <span
                 >
                   Blog
+                </span>
+              </span>
+            </Link>
+          </li>
+          <li className="group relative " onClick={() => setToggle(false)}>
+            <Link href="/iletisim">
+              <span className="text-jacarta-700 font-display hover:text-accentPam focus:text-accentPam flex items-center justify-between py-3.5 text-base dark:text-jacarta-700 lg:px-5 w-full cursor-pointer">
+                <span
+                >
+                  İletişimm
                 </span>
               </span>
             </Link>
